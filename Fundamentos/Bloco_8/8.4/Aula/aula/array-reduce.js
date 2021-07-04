496c610925b72debe0a73a3eccadd7f996a49655
Olavio Timoteo <olaviotimoteo@hotmail.com>
2021-07-04 10:32:45 -0300

    Atualiza exercicios

--- Fundamentos/Bloco_8/8.4/Aula/aula/array-reduce.js
+++ Fundamentos/Bloco_8/8.4/Aula/aula/array-reduce.js
@@ -4,3 +4,117 @@ const numbers = [32, 15, 3, 2, -5, 56, 10];
 const sumNumbers = numbers.reduce((result, number) => result + number);
 
 console.log(sumNumbers);
+
+//-------------------------------------------------------------------------------------------------------------------\\
+
+// Exemplo 2:
+
+const numbers = [32, 15, 3, 2, -5, 56, 10];
+
+const getSum = (result, number) => {
+  console.log(result); // 0 32 47 50 52 47 103
+  return result + number;
+};
+const sumNumbers = numbers.reduce(getSum, 0); // Parâmetro adicionado ao reduce: o "0"
+console.log(sumNumbers); // 113
+
+//-------------------------------------------------------------------------------------------------------------------\\
+
+// Exemplo 3:
+
+const numbers = [50, 85, -30, 3, 15];
+
+const getBigger = (bigger, number) => ((bigger > number) ? bigger : number);
+
+const bigger = numbers.reduce(getBigger, 0);
+console.log(bigger); // 85
+
+//-------------------------------------------------------------------------------------------------------------------\\
+
+// Exemplo 4:
+
+const estudantes = [{
+    nome: 'Jorge',
+    sobrenome: 'Silva',
+    idade: 14,
+    turno: 'Manhã',
+    materias: [
+      { name: 'Matemática', nota: 67 },
+      { name: 'Português', nota: 79 },
+      { name: 'Química', nota: 70 },
+      { name: 'Biologia', nota: 65 },
+    ],
+  },
+  {
+    nome: 'Mario',
+    sobrenome: 'Ferreira',
+    idade: 15,
+    turno: 'Tarde',
+    materias: [
+      { name: 'Matemática', nota: 59 },
+      { name: 'Português', nota: 80 },
+      { name: 'Química', nota: 78 },
+      { name: 'Biologia', nota: 92 },
+    ],
+  },
+  {
+    nome: 'Jorge',
+    sobrenome: 'Santos',
+    idade: 15,
+    turno: 'Manhã',
+    materias: [
+      { name: 'Matemática', nota: 76 },
+      { name: 'Português', nota: 90 },
+      { name: 'Química', nota: 70 },
+      { name: 'Biologia', nota: 80 },
+    ],
+  },
+  {
+    nome: 'Maria',
+    sobrenome: 'Silveira',
+    idade: 14,
+    turno: 'Manhã',
+    materias: [
+      { name: 'Matemática', nota: 91 },
+      { name: 'Português', nota: 85 },
+      { name: 'Química', nota: 92 },
+      { name: 'Biologia', nota: 90 },
+    ],
+  },
+  {
+    nome: 'Natalia',
+    sobrenome: 'Castro',
+    idade: 14,
+    turno: 'Manhã',
+    materias: [
+      { name: 'Matemática', nota: 70 },
+      { name: 'Português', nota: 70 },
+      { name: 'Química', nota: 60 },
+      { name: 'Biologia', nota: 50 },
+    ],
+  },
+  {
+    nome: 'Wilson',
+    sobrenome: 'Martins',
+    idade: 14,
+    turno: 'Manhã',
+    materias: [
+      { name: 'Matemática', nota: 80 },
+      { name: 'Português', nota: 82 },
+      { name: 'Química', nota: 79 },
+      { name: 'Biologia', nota: 75 },
+    ],
+  },
+];
+
+const getBestClass = (acc, materia) => {
+  if (acc.nota > materia.nota) return acc;
+  return materia;
+};
+
+const reportBetter = (students) => students.map((student) => ({
+  name: student.nome,
+  materia: student.materias.reduce(getBestClass).name
+}));
+
+console.log(reportBetter(estudantes));
