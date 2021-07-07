455c4367381227bd19272b6ba7a88624c830b72f
Olavio Timoteo <olaviotimoteo@hotmail.com>
2021-07-07 15:27:20 -0300

    Adiciona Aula API

--- Fundamentos/Bloco_9/9.2/Aula/Application-Programming-Interface-01.js
+++ Fundamentos/Bloco_9/9.2/Aula/Application-Programming-Interface-01.js
@@ -1,1 +1,17 @@
 // Application Programming Interface (API)
+
+
+// O que é o JSON?
+// JSON significa JavaScript Object Notation -> É a fora de representarmos dados como objetos JavaScript.
+
+// Exemplo:
+// {
+//   "trybers": [{
+//     "name": "Lauro Cesar",
+//     "github": "lauroandrade",
+//     "status": "#vqv"
+//   }]
+// }
+
+// Perceba o quão legível é o formato JSON, já que nossos dados ficam guardados como pares de key/value . A key fica no lado esquerdo e o value no lado direito.
+// No array trybers, podem ser adicionados vários outros objetos.
