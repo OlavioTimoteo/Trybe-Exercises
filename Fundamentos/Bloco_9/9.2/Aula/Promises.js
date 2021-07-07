6a2f25972124f4948306f75518bd5c661abfc8e2
Olavio Timoteo <olaviotimoteo@hotmail.com>
2021-07-07 16:19:39 -0300

    Finaliza Aula

---
+++ Fundamentos/Bloco_9/9.2/Aula/Promises.js
@@ -0,0 +1,16 @@
+// Criar uma Promise usando constructor:
+
+const promise = new Promise((resolve, reject) => {
+
+});
+
+// Exemplo utilizando Math.random()
+
+const promise = new Promise((resolve, reject) => {
+  const number = Math.floor(Math.random() * 11);
+
+  if (number <= 5) {
+    return reject(console.log(`Que fracasso =( Nosso número foi ${number}`));
+  }
+  resolve(console.log(`Que sucesso =) nosso número foi ${number}`));
+});
