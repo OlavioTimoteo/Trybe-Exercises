cbb0bbfeb586a7a8165b027bebfb1664423f029a
Olavio Timoteo <olaviotimoteo@hotmail.com>
2021-07-11 00:27:47 -0300

    Adiciona exercicio 9.2

---
+++ Fundamentos/Bloco_9/9.2/Exercicios/script.js
@@ -0,0 +1,40 @@
+const API_URL = 'https://icanhazdadjoke.com/';
+
+const fetchJoke = () => {
+  fetch(API_URL, { headers: { Accept: 'application/json' } })
+    .then(response => response.json())
+    .then(data =>
+      document.getElementById('jokeContainer').innerText = data.joke
+    );
+
+};
+const sumRandomNumbers = () => {
+  const myArray = Array.from({ length: 10 },
+    () => Math.floor(Math.random() * 50) + 1
+  );
+  const sum = myArray.map(number => number * number)
+    .reduce((number, acc) => number + acc, 0);
+
+  if (sum >= 8000) {
+    throw new Error();
+  }
+
+  return sum;
+}
+
+const sumArrayFromSum = (sum) => [2, 3, 5, 10].map(number => sum / number)
+  .reduce((number, acc) => number + acc);
+
+const fetchPromise = async() => {
+  try {
+    const sum = await sumRandomNumbers();
+    const sumFromSum = await sumArrayFromSum(sum);
+  } catch (error) {
+    console.log('É mais de oito mil! Essa promise deve estar quebrada!');
+  }
+}
+
+fetchPromise();
+
+
+window.onload = () => fetchJoke();
