e5bfe785f77d617d56e0e574ece81de0e35827d0
Olavio Timoteo <olaviotimoteo@hotmail.com>
2021-07-13 23:00:25 -0300

    Adiciona exercicios 10.1

--- Fundamentos/Bloco_10/10.1/Exercicios/Parte_1/Função-sum/sum.js
+++ Fundamentos/Bloco_10/10.1/Exercicios/Parte_1/Função-sum/sum.js
@@ -1,10 +1,9 @@
-function sum(a, b) {
-  if (typeof
-     a !== 'number' || typeof b !== 'number') {
-       throw new Error('parameters must be numbers');
-     }
-     return a + b;
+// sum.js
+const sum = (a, b) => {
+  if (typeof (a) !== 'number' || typeof (b) !== 'number') {
+    throw new Error('parameters must be numbers');
+  };
+  return a + b;
 };
-//console.log(sum(1, 4));
 
 module.exports = sum;
--- Fundamentos/Bloco_10/10.1/Exercicios/Parte_1/Função-sum/sum.spec.js
+++ Fundamentos/Bloco_10/10.1/Exercicios/Parte_1/Função-sum/sum.spec.js
@@ -1,10 +1,20 @@
 const sum = require('./sum');
 
-describe('Requisito 1', () => {
+describe('Função sum', () => {
   it('A função soma A e B e retorna 9', () => {
     expect(9).toEqual(sum(4, 5));
   });
   it('O retorno da soma de A e B é 0', () => {
     expect(0).toEqual(sum(0, 0))
   });
+  it('O retorno é a mensagem: throws an error when a string is passed', () => {
+    expect(() => {
+      sum(4, '5');
+    }).toThrow();
+  });
+  it('error message is "parameters must be numbers', () => {
+    expect(() => {
+      sum(4, '5');
+    }).toThrow(/parameters must be numbers/);
+  })
 });
