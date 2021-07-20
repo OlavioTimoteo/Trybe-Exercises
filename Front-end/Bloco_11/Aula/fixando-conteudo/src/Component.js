29ae4d990726296b353cd2797cc051c5c3d79527
Olavio Timoteo <olaviotimoteo@hotmail.com>
2021-07-20 15:49:04 -0300

    Adding lessons learned from the day

---
+++ Front-end/Bloco_11/Aula/fixando-conteudo/src/Component.js
@@ -0,0 +1,14 @@
+import React, {Component} from 'react'
+
+class Hello extends Component {
+  render() {
+    return <span>
+    <div>
+    <h1>Olávio Timóteo</h1>
+    <p> Estudante de desenvolvimento web</p>
+    </div>
+    </span>
+  };
+};
+  
+export default Hello
