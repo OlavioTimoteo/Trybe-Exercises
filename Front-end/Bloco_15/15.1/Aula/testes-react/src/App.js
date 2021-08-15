846fe88d1369b29e3719980abb5d60c3866f5f16
Olavio Timoteo <olaviotimoteo@hotmail.com>
2021-08-15 12:39:01 -0300

    First concepts about RTL

--- Front-end/Bloco_15/15.1/Aula/testes-react/src/App.js
+++ Front-end/Bloco_15/15.1/Aula/testes-react/src/App.js
@@ -1,23 +1,15 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <label htmlFor="id-email">
+        Email
+      </label>
+      <input id="id-email" type="email" />
+      <input id="btn-send" type="button" data-testid="id-send" value="Enviar" />
+      <input id="btn-back" type="button" data-testid="id-back" value="Voltar" />
     </div>
   );
 }
--- Front-end/Bloco_15/15.1/Aula/testes-react/src/App.test.js
+++ Front-end/Bloco_15/15.1/Aula/testes-react/src/App.test.js
@@ -1,8 +1,35 @@
-import { render, screen } from '@testing-library/react';
+import React from 'react';
+import { getByLabelText, render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
+describe('Tela de inserção do email', () => {
+it('Verifica se existe um campo de Email', () => {
+    const { getByLabelText } = render(<App />);
+    const inputEmail = getByLabelText('Email');
+    expect(inputEmail).toBeInTheDocument();
+    expect(inputEmail.type).toBe('email');
+  });
+
+
+  it('Verifica se existem dois botões', () => {
+    const { getAllByRole } = render(<App />);
+    const buttons = getAllByRole('button')
+    expect(buttons.length).toBe(2);
+  })
+
+  it('Verifica se existe um botão "Enviar"', () => {
+    const { getByTestId } = render(<App />);
+    const btn = getByTestId('id-send');
+    expect(btn).toBeInTheDocument();
+    expect(btn.type).toBe('button');
+    expect(btn).toHaveValue('Enviar');
+  });
+
+  it('Verifica se existe um botão "Voltar"', () => {
+    const { getByTestId } = render(<App />);
+    const btn = getByTestId('id-back');
+    expect(btn).toBeInTheDocument();
+    expect(btn.type).toBe('button');
+    expect(btn).toHaveValue('Voltar');
+  });
+})
