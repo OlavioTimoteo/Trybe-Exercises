d4e0703d01da87bcfc45f06c5a8b24cba829be5d
Olavio Timoteo <olaviotimoteo@hotmail.com>
2021-08-26 23:52:36 -0300

    finished

---
+++ Front-end/Bloco_16/16.1/Exercicios/Exercício2/script.js
@@ -0,0 +1,53 @@
+const meuPrimeiroReducer = (state = ESTADO_INICIAL_1, action) => {
+  switch (action.type) {
+    case 'ALTERAR_PRIMEIRO_NOME_E_SOBRENOME':
+      return {
+        ...state,
+        nome: action.nome,
+        sobrenome: action.sobrenome,
+      };
+    default:
+      return state;
+  }
+};
+
+const meuSegundoReducer = (state = ESTADO_INICIAL_2, action) => {
+switch (action.type) {
+  case 'ALTERAR_SEGUNDO_NOME_E_SOBRENOME':
+    return {
+      ...state,
+      nome: action.nome,
+      sobrenome: action.sobrenome,
+    };
+  default:
+    return state;
+}
+};
+
+const rootReducer = Redux.combineReducers({ meuPrimeiroReducer, meuSegundoReducer });
+
+const store = Redux.createStore(rootReducer);
+
+window.onload = () => {
+setTimeout(() => {
+  store.dispatch({
+    type: 'ALTERAR_PRIMEIRO_NOME_E_SOBRENOME',
+    nome: 'Bruna',
+    sobrenome: 'Santana Oliveira',
+  });
+  store.dispatch({
+    type: 'ALTERAR_SEGUNDO_NOME_E_SOBRENOME',
+    nome: 'Rodrigo',
+    sobrenome: 'Santos da Silva',
+  });
+}, 2000);
+};
+
+store.subscribe(() => {
+const { meuPrimeiroReducer, meuSegundoReducer } = store.getState();
+document.getElementById('nome-1').innerHTML = meuPrimeiroReducer.nome;
+document.getElementById('sobrenome-1').innerHTML = meuPrimeiroReducer.sobrenome;
+
+document.getElementById('nome-2').innerHTML = meuSegundoReducer.nome;
+document.getElementById('sobrenome-2').innerHTML = meuSegundoReducer.sobrenome;
+});
